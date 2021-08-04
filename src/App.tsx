dfcb73c504674ceefb8461e6fd772584ca5ed4a3
devpinetree <bosollee@gmail.com>
2021-08-04 21:40:06 +0900

    ADD: 타입스크립트 환경에서 인풋 상태 관리하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Greetings from './Greetings';
 import Counter from './Counter';
+import MyForm from './MyForm';
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
     console.log(`${name} clicked button`);
   };
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
   return (
     <>
       <Greetings name="me" onClick={onClick} />
       <Counter />
+      <MyForm onSubmit={onSubmit} />
     </>
   );
 };
