ac0654edd41bd8db1005af7b80c09b5c23d12899
devpinetree <bosollee@gmail.com>
2021-08-04 21:50:08 +0900

    ADD: 타입스크립트 환경에서 useRef 사용

--- src/MyForm.tsx
+++ src/MyForm.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 type MyFormProps = {
   onSubmit: (form: { name: string; description: string }) => void;
 };
 
 function MyForm({ onSubmit }: MyFormProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const [form, setForm] = useState({
     name: '',
     description: '',
@@ -14,7 +16,6 @@ function MyForm({ onSubmit }: MyFormProps) {
 
   // e 값을 무엇으로 설정할지 모를 때 any로 설정.
   //   const onChange = (e: any) => {
-
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setForm({
@@ -30,11 +31,15 @@ function MyForm({ onSubmit }: MyFormProps) {
       name: '',
       description: '',
     }); // 초기화
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.focus();
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input name="name" value={name} onChange={onChange} />
+      <input name="name" value={name} onChange={onChange} ref={inputRef} />
       <input name="description" value={description} onChange={onChange} />
       <button type="submit">등록</button>
     </form>
